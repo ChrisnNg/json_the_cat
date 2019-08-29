82e7e25bfc3311a832e7f343b76bd9e72cd0b2b1
ChrisnNg <cristopherng@hotmail.com>
2019-08-29 22:57:33 +0000

    breedFetcher.js now configured to be run off index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,13 @@
 const request = require('request');
-const getArgv = process.argv.splice(2);
-const fetch = function(breed) {
-  request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
+
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
     if (!error) {
-      if (body.length > 2) {
-        console.log(JSON.parse(body)[0]["description"]);
-      } else console.log("Request breed unable to be found. Search cancelled.");
-    } else console.log('error! Request Failed.\n', error);
+      if (body.length !== 2) {
+        callback(null, JSON.parse(body)[0]["description"]);
+      } else console.log("Requested breed unable to be found. Search cancelled.");
+    } else callback(null, `Error! Fetch Request Failed.\n${error}`);
   });
 };
 
-fetch(getArgv[0]);
+module.exports = fetchBreedDescription;
