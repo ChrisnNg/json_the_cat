dc7be56cee3d7f4ca8213216f2569633f95823e8
ChrisnNg <cristopherng@hotmail.com>
2019-08-29 20:14:14 +0000

    both edge cases implemented

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,15 +2,12 @@ const request = require('request');
 const getArgv = process.argv.splice(2);
 const fetch = function(breed) {
   request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
-    console.log('error', error);
-    console.log('statusCode', response.statusCode);
-    console.log('body', body, body.length);
-    if (body.length > 2) {
-      console.log(JSON.parse(body)[0]["description"]);
-    } else console.log("Request breed unable to be found. Search cancelled.");
+    if (!error) {
+      if (body.length > 2) {
+        console.log(JSON.parse(body)[0]["description"]);
+      } else console.log("Request breed unable to be found. Search cancelled.");
+    } else console.log('error! Request Failed.\n', error);
   });
-
 };
 
 fetch(getArgv[0]);
-
