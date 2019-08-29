3c624cf216a5c4a1dfa3516d5e6494a5e49ae4b1
ChrisnNg <cristopherng@hotmail.com>
2019-08-29 23:23:07 +0000

    completed

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,10 +4,11 @@ const fetchBreedDescription = function(breedName, callback) {
   request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
     if (!error) {
       if (body.length !== 2) {
-        callback(null, JSON.parse(body)[0]["description"]);
-      } else console.log("Requested breed unable to be found. Search cancelled.");
+        callback(null, JSON.parse(body)[0]["description"].trim());
+        return;
+      } else callback(null, `Requested breed unable to be found. Search cancelled.`);
     } else callback(null, `Error! Fetch Request Failed.\n${error}`);
   });
 };
 
-module.exports = fetchBreedDescription;
+module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // index.js
-const fetchBreedDescription = require('./breedFetcher');
+const { fetchBreedDescription } = require('./breedFetcher');
 
 const breedName = process.argv[2];
 
 fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
-    console.log('Error fetch details:', error);
+    console.log('Error fetching details:', error);
   } else {
     console.log(desc);
   }
